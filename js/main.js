b5856fbfdcd36a1b97dc8b53f57fb76799c513dc
Cathy Garcia <garciacathy67@yahoo.com>
2023-08-19 17:49:50 +0000

    Updated css styles and created event listener for search bar.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 const $images = document.querySelector('.images');
+const $search = document.querySelector('.search');
+
+function formatDate(inputDate) {
+  const parts = inputDate.split('-');
+  const year = parts[0];
+  const month = parts[1];
+  const day = parts[2];
+  return `${month}/${day}/${year}`;
+}
 
 function getImage(DMA) {
   const xhr = new XMLHttpRequest();
@@ -9,6 +18,17 @@ function getImage(DMA) {
     for (let i = 0; i < response.length; i++) {
       const $imgWrapper = document.createElement('div');
       const $img = document.createElement('img');
+
+      const $artistName = document.createElement('p');
+      $artistName.textContent = response[i].name;
+
+      const $venue = document.createElement('p');
+      $venue.textContent = response[i]._embedded.venues[0].name;
+
+      const $date = document.createElement('p');
+      const formattedDate = formatDate(response[i].dates.start.localDate);
+      $date.textContent = `${formattedDate}`; // Include 'Date: ' prefix
+
       for (let j = 0; j < response[i].images.length; j++) {
         const currentImage = response[i].images[j];
         if (currentImage.ratio === '4_3') {
@@ -17,15 +37,65 @@ function getImage(DMA) {
       }
 
       $imgWrapper.appendChild($img);
+      $imgWrapper.appendChild($artistName);
+      $imgWrapper.appendChild($venue);
+      $imgWrapper.appendChild($date);
+      $imgWrapper.classList.add('column-third');
       $images.appendChild($imgWrapper);
+
     }
   });
   xhr.send();
 }
 
+$search.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    const xhr = new XMLHttpRequest();
+    const searchValue = event.target.value;
+    xhr.open('GET', `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=${searchValue}&size=30&apikey=aeMvG0zyzdpO1jAkGyCZeGxxQK4vIfpe`);
+    xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      const response = xhr.response._embedded.events;
+      // console.log(response);
+      $images.innerHTML = '';
+
+      for (let i = 0; i < response.length; i++) {
+        const $imgWrapper = document.createElement('div');
+        const $img = document.createElement('img');
+
+        const $artistName = document.createElement('p');
+        $artistName.textContent = response[i].name;
+
+        const $venue = document.createElement('p');
+        $venue.textContent = response[i]._embedded.venues[0].name;
+
+        const $date = document.createElement('p');
+        const formattedDate = formatDate(response[i].dates.start.localDate);
+        $date.textContent = `${formattedDate}`; // Include 'Date: ' prefix
+
+        for (let j = 0; j < response[i].images.length; j++) {
+          const currentImage = response[i].images[j];
+          if (currentImage.ratio === '4_3') {
+            $img.src = currentImage.url;
+          }
+        }
+
+        $imgWrapper.appendChild($img);
+        $imgWrapper.appendChild($artistName);
+        $imgWrapper.appendChild($venue);
+        $imgWrapper.appendChild($date);
+        $imgWrapper.classList.add('column-third');
+        $images.appendChild($imgWrapper);
+
+      }
+
+    });
+    xhr.send();
+  }
+});
+
 const dmaArray = [
   '324',
-  '319',
   '222',
   '228',
   '300',
@@ -44,9 +114,5 @@ const dmaArray = [
 ];
 
 const randomIndex = Math.floor(Math.random() * dmaArray.length);
-
 const randomDMA = dmaArray[randomIndex];
-
-// console.log('randomDMA', randomDMA);
-
 getImage(randomDMA);
